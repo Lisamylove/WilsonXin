7d371a47adebb152757e788a0eceba929d6bd73e
Jack <597910268@qq.com>
2018-10-15 11:30:32 +0800

    点击跳转至CurrentLabelActivity流式标签页面

--- RequestDemo/app/src/main/java/com/example/wilsonxin/requestdemo/ButtonActivity.java
+++ RequestDemo/app/src/main/java/com/example/wilsonxin/requestdemo/ButtonActivity.java
@@ -7,27 +7,35 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 
-public class ButtonActivity extends Activity {
+public class ButtonActivity extends Activity implements View.OnClickListener {
     private Button ok_get;
+    private Button bt_button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
         initView();//找控件
-        ButtonOnClick();//按钮设置点击事件
+        //设置点击事件
+        bt_button.setOnClickListener(this);
+        ok_get.setOnClickListener(this);
     }
-
-    private void ButtonOnClick() {
-        ok_get.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(ButtonActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
     private void initView() {
         ok_get = findViewById(R.id.Ok_post);
+        bt_button = findViewById(R.id.bt_button);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Ok_post:
+                Intent intent=new Intent(ButtonActivity.this,MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.bt_button:
+                Intent intent2=new Intent(ButtonActivity.this,CurrentLabelActivity.class);
+                startActivity(intent2);
+                break;
+        }
     }
 }
